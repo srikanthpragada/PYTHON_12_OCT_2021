48786ba8221cffdb32ebad1daf1008c912aa4dbf
srikanthpragada <srikanthpragada@gmail.com>
2021-11-03 19:56:26 +0530

    03-NOV

--- demo/modules/test.py
+++ demo/modules/test.py
@@ -1,4 +1,10 @@
 
+import sys
+
+print(sys.path)   # Module search path
+
+sys.path.insert(0, r"c:\classroom\oct12\demo\lib")
+
 import numbers as nf
 from numbers import ispositive
 
---
+++ demo/modules/use_lib.py
@@ -0,0 +1,9 @@
+
+# from lib import string_funs as sf
+#
+# print(sf.has_upper("abc"))
+
+
+from lib import *
+
+print(string_funs.starts_with_digit('223'))
