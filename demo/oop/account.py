469534ad4961a7ed2ac94075ec2b08a19adc466c
srikanthpragada <srikanthpragada@gmail.com>
2021-11-06 19:55:25 +0530

    06-NOV

--- demo/oop/account.py
+++ demo/oop/account.py
@@ -1,10 +1,18 @@
 class Account:
+    # static attribute or class attribute
+    minbal = 5000
+
+    @staticmethod
+    def getminbal():
+        return Account.minbal
+
     # Constructor
     def __init__(self, no, name, amount=0):
         # object attributes
         self.acno = no
         self.ahname = name
         self.balance = amount
+
 
     def show(self):
         print(self.acno)
@@ -15,17 +23,25 @@ class Account:
         self.balance += amount
 
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.balance - Account.minbal >= amount:
+            self.balance -= amount
+        else:
+            raise ValueError("Insufficient Funds!")
+
+    def getbalance(self):
+        return self.balance
 
 
+print('Min bal ', Account.minbal)
 # create object
 a1 = Account(1, "Steve")
 a1.deposit(10000)
 a1.deposit(40000)
-print(a1.balance)
+print(a1.getbalance())
 
 # Call method
 a1.show()
 
 a2 = Account(2, "Mark", 10000)
+a2.withdraw(20000)
 a2.show()
---
+++ demo/oop/special_methods.py
@@ -0,0 +1,28 @@
+class Person:
+    def __init__(self, name, age):
+        # Private attributes
+        self.name = name
+        self.age = age
+
+    def getname(self):
+        return self.name
+
+    def getage(self):
+        return self.age
+
+    def __str__(self):
+        return f"{self.name} - {self.age}"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+
+p1 = Person("Abc", 20)
+print(p1)  # str(p1)  --> p1.__str__()
+p2 = Person("Abc", 20)
+print(p1 == p2)  # p1.__eq__(p2)
+p3 = Person("Xyz", 30)
+print(p3 > p1)   # p3.__gt__(p1)
